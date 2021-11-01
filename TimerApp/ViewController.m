f11e0e16eb2d21d9d2f8ea413af979416c393f46
MrGcGamer <32813275+MrGcGamer@users.noreply.github.com>
2021-11-02 00:59:53 +0100

    RunLoop moment :fr:

--- TimerApp/ViewController.m
+++ TimerApp/ViewController.m
@@ -8,10 +8,11 @@
 #import "ViewController.h"
 
 @interface ViewController () {
-    NSTimer *_timer;
+    NSDate *_beginning;
     BOOL _runnin;
-    NSUInteger _count;
     NSUserDefaults *_prefs;
+    NSTimeInterval _suffering;
+    NSTimer *_timer;
 }
 @end
 
@@ -21,39 +22,39 @@
     [super viewDidLoad];
     
     _prefs = [NSUserDefaults standardUserDefaults];
-    _count = ([_prefs integerForKey:@"Count"]) ?: 0;
-    NSString *timeString = [self stringForCount:_count];
+    _suffering = ([_prefs floatForKey:@"Count"]) ?: 0;
+    NSString *timeString = [self stringForCount:_suffering];
     [self.TimerLabel setStringValue:timeString];
 
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(term) name:NSWindowWillCloseNotification object:nil];
-    // Do any additional setup after loading the view.
 }
 
 - (void)term {
-    exit(0);
-}
-
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
-}
-
-- (IBAction)Start:(NSButton *)sender {
     if (_runnin) {
         _runnin = NO;
         [_timer invalidate];
+        _suffering = -[_beginning timeIntervalSinceNow];
+        [_prefs setValue:[NSNumber numberWithFloat:_suffering] forKey:@"Count"];
+    }
+    exit(0);
+}
+
+- (IBAction)Start:(NSButton *)sender {
+    if (_runnin) { // stop it, get some help
+        _runnin = NO;
+        [_timer invalidate];
+        _suffering = -[_beginning timeIntervalSinceNow];
         self.Start.title = @"Start";
-        [_prefs setValue:[NSNumber numberWithUnsignedLong:_count] forKey:@"Count"];
+        [_prefs setValue:[NSNumber numberWithFloat:_suffering] forKey:@"Count"];
     } else {
         _runnin = YES;
-        _timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerCounter) userInfo:nil repeats:YES];
+        _timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateCounter) userInfo:nil repeats:YES];
+        _beginning = [NSDate dateWithTimeIntervalSinceNow:-_suffering];
         self.Start.title = @"Stop";
     }
 }
 - (IBAction)Reset:(NSButton *)sender {
     NSAlert *alert = [NSAlert new];
-//    alert.delegate = self;
     alert.alertStyle = NSAlertStyleWarning;
     alert.icon = [NSImage imageNamed:@"AppIcon"];
     alert.messageText = @"Are you sure, you wanna end it all for good?";
@@ -65,9 +66,8 @@
     NSInteger pill = [alert runModal];
     
     if (pill == NSAlertFirstButtonReturn) { // red pill
-        _count = 0;
         _runnin = NO;
-        [_timer invalidate];
+        _suffering = 0;
         self.Start.title = @"Start";
         [self.TimerLabel setStringValue:@"00 : 00 : 00"];
         [_prefs setValue:@0 forKey:@"Count"];
@@ -82,8 +82,9 @@
     return ret;
 }
 
-- (void)timerCounter {
-    NSString *timeString = [self stringForCount:++_count];
+- (void)updateCounter {
+    NSTimeInterval time = -[_beginning timeIntervalSinceNow];
+    NSString *timeString = [self stringForCount:time];
     [self.TimerLabel setStringValue:timeString];
 }
 
